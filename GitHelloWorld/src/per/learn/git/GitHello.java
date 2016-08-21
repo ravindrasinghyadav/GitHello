34641dddbe13b185c447352a918dc75d0509f687
Ravindra <Ravindra.yadav02@gmail.com>
2016-08-21 21:41:13 +0100

    Initial checkin of gitHello

---
+++ GitHelloWorld/src/per/learn/git/GitHello.java
@@ -0,0 +1,14 @@
+package per.learn.git;
+
+public class GitHello {
+	
+	public static void main(String[] args){
+		
+		System.out.println("So now you are in main...");
+		
+		GitNext gitnxt=new GitNext();
+		
+		gitnxt.printgitNext();
+	}
+
+}
---
+++ GitHelloWorld/src/per/learn/git/GitNext.java
@@ -0,0 +1,9 @@
+package per.learn.git;
+
+public class GitNext {
+
+	void printgitNext(){
+		System.out.println("You are in Git next..wow!!s");
+	}
+	
+}
